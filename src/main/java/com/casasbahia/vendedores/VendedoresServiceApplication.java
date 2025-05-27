44eb7f89e60f361aeb8b8495aceb1d4a0ef19a18
Rodolfosaw <rodolfo.medeirossp@outlook.com>
2025-05-27 14:15:50 -0300

    feature: controller e service

--- src/main/java/com/casasbahia/vendedores/VendedoresServiceApplication.java
+++ src/main/java/com/casasbahia/vendedores/VendedoresServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VendedoresServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VendedoresServiceApplication.class, args);
     }
--- src/main/java/com/casasbahia/vendedores/service/VendedorService.java
+++ src/main/java/com/casasbahia/vendedores/service/VendedorService.java
@@ -1,4 +1,12 @@
 package com.casasbahia.vendedores.service;
 
-public class VendedorService {
+import com.casasbahia.vendedores.dto.VendedorDTO;
+import java.util.List;
+
+public interface VendedorService {
+    List<VendedorDTO> findAll();
+    VendedorDTO findById(Long id);
+    VendedorDTO create(VendedorDTO dto);
+    VendedorDTO update(Long id, VendedorDTO dto);
+    void delete(Long id);
 }
